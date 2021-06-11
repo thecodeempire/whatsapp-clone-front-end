239992dcdf48214f724ebb16c660c09828fc6c67
Himujjal <himu@tuta.io>
2021-06-12 03:50:44 +0530

    updated configs

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 const config = {
   baseURL: process.env.PUBLIC_URL || "https://whatsapp-clone-simple.herokuapp.com",
+  // baseURL: 'http://localhost:9000',
   pusherId: "92227230285a5ac11574"
 }
 
